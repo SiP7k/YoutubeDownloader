221a753c1a110b91bf77a6ee06e7bde18b677383
Савва Чальцев <cavvach@gmail.com>
2023-03-21 14:12:19 -0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace YoutubeDownloader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
